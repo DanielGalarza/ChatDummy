5703860f5c6bf02b0c7548bc9119ea7b3188f200
taky2 <ta.kymatica@gmail.com>
2015-11-19 20:44:29 -0700

    planning (change list items)

--- app/src/main/java/com/example/danielgalarza/chatdummy/MainActivity.java
+++ app/src/main/java/com/example/danielgalarza/chatdummy/MainActivity.java
@@ -197,13 +197,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_todolist) {
             //Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_maps) {
+
+        } else if (id == R.id.nav_chat) {
 
         } else if (id == R.id.nav_share) {
 
